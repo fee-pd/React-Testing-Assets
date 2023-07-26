bf7f91b48e96942015558529f8bead7d6aa6caae
fee-pd <104866604+fee-pd@users.noreply.github.com>
2023-07-26 13:33:13 -0600

    Delete packages/basic-testing/src directory

--- packages/basic-testing/src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from './utils/test-utils';
-import App from './App';
-
-describe('App', () => {
-  test('renders learn react link', () => {
-    render(<App />);
-  });
-});
--- packages/basic-testing/src/components/2.Presentational-components/expected-properties/ExpectedProperties.component.test.tsx
+++
@@ -1,7 +0,0 @@
-import ExpectedProperties from './ExpectedProperties.component';
-import { render } from 'src/utils/test-utils';
-
-
-it('displays the heading', () => {
-  render(<ExpectedProperties />);
-});
